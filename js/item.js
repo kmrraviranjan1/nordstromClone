236c0f040d64f10f5e7fbeb4c2dc626db6b5a969
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-06-13 23:41:30 +0530

    items icon and image getting dynamically updated

--- js/item.js
+++ js/item.js
@@ -4,6 +4,47 @@ let title = document.querySelector('title');
 title.innerHTML = item.name;
 
 
+let iconParent = document.getElementById('icon-parent');
+
+let imageIcon = document.getElementById('img-icon')
+imageIcon.onclick = function () {
+    console.log(55)
+    mainImg.src = item.image
+    
+}
+imageIconImg = document.createElement('img');
+imageIconImg.src = item.imageIcon
+imageIcon.appendChild(imageIconImg)
+
+let quickImageIcon = document.getElementById('quick-img-icon')
+quickImageIcon.onclick=function(){
+    mainImg.src=item.quickImage
+}
+quickImageIconImg = document.createElement('img');
+quickImageIconImg.src = item.quickImageIcon
+quickImageIcon.appendChild(quickImageIconImg)
+
+let videoIcon = document.getElementById('video-icon')
+
+videoIconImg = document.createElement('img');
+videoIconImg.src = item.videoIcon
+videoIcon.appendChild(videoIconImg)
+
+let rawMaterialImageIcon = document.getElementById('raw-icon')
+rawMaterialImageIcon.onclick=function(){
+    mainImg.src= item.rawMaterialImage
+}
+rawMaterialImageIconImg = document.createElement('img');
+rawMaterialImageIconImg.src = item.rawMaterialImageIcon
+rawMaterialImageIcon.appendChild(rawMaterialImageIconImg)
+
+let mainImage = document.getElementById('main-img');
+let mainImg = document.createElement('img');
+mainImg.src = item.image;
+mainImage.appendChild(mainImg)
+
+
+
 let sizeInform = document.getElementById('size-inform');
 let info = item.sizeInfo;
 let sizeUl = document.getElementById('size-ul')
