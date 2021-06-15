9c00b543655a9ed92adc18e3292d6b79b6284fec
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-06-16 01:01:23 +0530

    sorting functionality added in men clothing section

--- js/menClothing.js
+++ js/menClothing.js
@@ -21,9 +21,9 @@ const cloth = [
         rawMaterialImage: "https://n.io.nordstrommedia.com/id/sr3/1fe9de9d-9571-4d3a-87bc-8383e736a4e7.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=860&h=1318&dpr=2",
         rawMaterialImageIcon:"https://n.io.nordstrommedia.com/id/sr3/1fe9de9d-9571-4d3a-87bc-8383e736a4e7.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=60&h=90",
         videoIcon:"https://n.io.nordstrommedia.com/id/sr3/2ca0416c-aade-455a-a7a9-2f02c4190c20.jpeg",
-        price: "2514.76",
+        price: "25  ",
         rating: "4",
-        raters: "135",
+        raters: "35",
         sizeInfo: ['Classic rise.', 'True to size.', 'Order by your waist size; S=28-30", M=31-34", L=35-38", XL=40-42", XXL=43-45".'],
         care: ['11" inseam; 22" leg opening; 13" front rise; 14" back rise (size Medium).','60% Pima cotton, 40% modal.','Machine wash cold, tumble dry low.','By Polo Ralph Lauren; imported.',"Men's Furnishings.",'Item #795807'],
         brandDesc: 'New York City native Ralph Lauren began his career designing and selling ties, laying the foundation to start his own company in 1967. Now a fashion empire made up of a family of brands, Ralph Lauren represents a distinctive American perspective rooted in an aspirational lifestyle. Polo Ralph Lauren was launched in 1968, offering designs that are clean, timeless and ultrawearable.'
@@ -50,9 +50,9 @@ const cloth = [
         rawMaterialImage: "https://n.io.nordstrommedia.com/id/sr3/a0de479a-e188-486d-9db0-9c1be1e6381e.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196&dpr=2",
         rawMaterialImageIcon:"https://n.io.nordstrommedia.com/id/sr3/a0de479a-e188-486d-9db0-9c1be1e6381e.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=60&h=90",
         videoIcon:"",
-        price: "5461.74",
-        rating: "****",
-        raters: "(135)",
+        price: "54",
+        rating: "5",
+        raters: "80",
         sizeInfo: ['True to size.'],
         care: ['Front button closure','Button-down collar','Long sleeves with button cuffs','Chest patch pocket','100% linen','Machine wash, tumble dry','Imported',"Men's Clothing"," Item #6122094"],
         brandDesc: 'The Nordstrom line of high-quality clothing, shoes and accessories offers just the right pieces for women, men and kids seeking timeless, classic items to complement and polish their wardrobe. Versatility, ease and affordability are hallmarks of the Nordstrom collection. Available exclusively at Nordstrom.'
@@ -79,9 +79,9 @@ const cloth = [
         rawMaterialImage: "https://n.io.nordstrommedia.com/id/sr3/b9858781-b775-4cde-ab0e-3b0cb27d29b0.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196&dpr=2",
         rawMaterialImageIcon: "https://n.io.nordstrommedia.com/id/sr3/b9858781-b775-4cde-ab0e-3b0cb27d29b0.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=60&h=90",
         videoIcon:"https://n.io.nordstrommedia.com/id/sr3/9427c237-4d30-4d50-9398-d34224c8c33f.jpeg",
-        price: "10216.21",
-        rating: "****",
-        raters: "(5)",
+        price: "120",
+        rating: "2",
+        raters: "5",
         sizeInfo: ['If ordering for the first time; order one size up.', 'Extra-fitted through seat and thigh.', 'Tight through knee and ankle with a narrow leg opening.', 'Mid rise.'],
         care: ['31" inseam; 14" leg opening; 10 1/2" front rise; 14" back rise (size 32)','Zip fly with hook-and-bar closure','66% polyester, 34% viscose','Dry clean','Imported','Item #5707933'],
         brandDesc: "From British high-street darling to international phenom, Topman has delivered affordable, trend-right styles for men since 1978. Best known for cool clothes with a rock-and-roll attitude, Topman offerings include suits, jeans, shirts, jackets, accessories and more. Topman is a staunch supporter of UK menswear design talent, and their TOPMAN DESIGN collection opens London Fashion Week Men's every year."
@@ -108,9 +108,9 @@ const cloth = [
         rawMaterialImage: "https://n.io.nordstrommedia.com/id/sr3/f15806cb-4d25-49ce-b1cd-2f7165becec6.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
         rawMaterialImageIcon:"https://n.io.nordstrommedia.com/id/sr3/f15806cb-4d25-49ce-b1cd-2f7165becec6.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=60&h=90",
         videoIcon:"https://n.io.nordstrommedia.com/id/sr3/d8c2050e-59ab-4874-8ad7-5ee4a164de7e.jpeg",
-        price: "2781.95",
-        rating: "***",
-        raters: "(7)",
+        price: "67",
+        rating: "3",
+        raters: "7",
         sizeInfo: ['True to size.',],
         care: ['27 1/2" length (size Medium)','Drawstring hood','Ribbed cuffs and hem.','Kangaroo pocket',"58% cotton, 37% polyester, 5% spandex",'Machine wash, tumble dry','Imported',"Men's Clothing",'Item #6055001'],
         brandDesc: "What we wear prepares us for what's ahead, whether that's a walk in the rain, a moment of stillness or a hike up a mountain. Zella is created with comfort innovation and performance technology for women, men and kids—a wardrobe of progressive designs that make us feel strong, balanced, engaged in the now and ready for what's next. Life is lived in motion. Zella is made to move."
@@ -123,79 +123,163 @@ if (JSON.parse(localStorage.getItem('clothes')) == null) {
     localStorage.setItem('clothes',JSON.stringify(cloth))
 }
 
+
+
+
 let parent = document.getElementById('parent');
-
-let data = JSON.parse(localStorage.getItem('clothes'));
-
-data.forEach(function (element) {
-    let card = document.createElement('div');
-    card.setAttribute('class','card')
-    let imgcard = document.createElement('div');
-    imgcard.setAttribute('class','img-card')
-    let imgDiv = document.createElement('div');
-    imgDiv.setAttribute('class', 'img-div');
-    imgDiv.onclick = function () {
-        let item = localStorage.getItem('item');
-        item = null;
-        localStorage.setItem('item',JSON.stringify(element))
-        
-        
-        window.location.href = "../html/item.html"
-        
-    }    
-    let img = document.createElement('img');
-   
-    img.src = element.image;
-    imgDiv.appendChild(img);
-    let quickDiv = document.createElement('div');
-    quickDiv.setAttribute('class','quick-Div')
-    imgcard.append(imgDiv,quickDiv)
-
-    let desCard = document.createElement('div');
-    desCard.setAttribute('class','des-card')
-
-    let colorSelector = document.createElement('div');
-    colorSelector.setAttribute('class','color-selector')
-    let blue = document.createElement('div')
-    blue.setAttribute('class','blue')
-    let black = document.createElement('div')
-    black.setAttribute('class','black')
-    let yellow = document.createElement('div')
-    yellow.setAttribute('class','yellow')
-    let voilet = document.createElement('div')
-    voilet.setAttribute('class','voilet')
-    colorSelector.append(blue, black, yellow, voilet);
-
-    let tagDiv = document.createElement('div');
-    tagDiv.setAttribute('class','tag-div')
-    let tag = document.createElement('p');
     
-    tag.innerHTML = `${element.tag}`;
-    tagDiv.appendChild(tag);
 
-    let nameDiv = document.createElement('div');
-    nameDiv.setAttribute('class','name-div')
-    let name = document.createElement('p');
+let original = JSON.parse(localStorage.getItem('clothes'));
+
+let itemavailable = 0;
+appendItem(original);
+
+
+function appendItem(data) {
     
-    name.innerHTML = `${element.brand} ${element.name}`
-    nameDiv.appendChild(name);
-
-    let priceDiv = document.createElement('div');
-    priceDiv.setAttribute('class','price-div')
-    let price = document.createElement('p');
- 
-    price.innerHTML = `${element.price}`;
-    priceDiv.append(price);
-
-    let ratingDiv = document.createElement('div');
-    ratingDiv.setAttribute('class','rating-div')
-    let rating = document.createElement('p');
     
-    rating.innerHTML = `${element.rating} (${element.raters})`;
-    ratingDiv.appendChild(rating);
+    data.forEach(function (element) {
+        itemavailable++;
+        let card = document.createElement('div');
+        card.setAttribute('class','card')
+        let imgcard = document.createElement('div');
+        imgcard.setAttribute('class','img-card')
+        let imgDiv = document.createElement('div');
+        imgDiv.setAttribute('class', 'img-div');
+        imgDiv.onclick = function () {
+            let item = localStorage.getItem('item');
+            item = null;
+            localStorage.setItem('item',JSON.stringify(element))
+            
+            
+            window.location.href = "../html/item.html"
+            
+        }    
+        let img = document.createElement('img');
+    
+        img.src = element.image;
+        imgDiv.appendChild(img);
+        let quickDiv = document.createElement('div');
+        quickDiv.setAttribute('class','quick-Div')
+        imgcard.append(imgDiv,quickDiv)
 
-    desCard.append(colorSelector, tagDiv, nameDiv, priceDiv,ratingDiv);
+        let desCard = document.createElement('div');
+        desCard.setAttribute('class','des-card')
+        
+        let colorSelector = document.createElement('div');
+        colorSelector.setAttribute('class','color-selector')
+        let blue = document.createElement('div')
+        blue.setAttribute('class','blue')
+        let black = document.createElement('div')
+        black.setAttribute('class','black')
+        let yellow = document.createElement('div')
+        yellow.setAttribute('class','yellow')
+        let voilet = document.createElement('div')
+        voilet.setAttribute('class','voilet')
+        colorSelector.append(blue, black, yellow, voilet);
+        
+        let tagDiv = document.createElement('div');
+        tagDiv.setAttribute('class','tag-div')
+        let tag = document.createElement('p');
+        
+        tag.innerHTML = `${element.tag}`;
+        tagDiv.appendChild(tag);
 
-    card.append(imgcard, desCard);
-    parent.appendChild(card)
-})
+        let nameDiv = document.createElement('div');
+        nameDiv.setAttribute('class','name-div')
+        let name = document.createElement('p');
+        
+        name.innerHTML = `${element.brand} ${element.name}`
+        nameDiv.appendChild(name);
+        
+        let priceDiv = document.createElement('div');
+        priceDiv.setAttribute('class','price-div')
+        let price = document.createElement('p');
+        
+        price.innerHTML = `INR ${element.price*80}`;
+        priceDiv.append(price);
+
+        let ratingDiv = document.createElement('div');
+        ratingDiv.setAttribute('class','rating-div')
+        let rating = document.createElement('p');
+        let actualRate = Number(element.rating);
+        console.log('actualRate: ', actualRate);
+        let ratingString = "";
+        for (let i = 0; i < actualRate; i++){
+            ratingString= ratingString+"*"
+        }
+        
+        rating.innerHTML = `${ratingString} (${element.raters})`;
+        ratingDiv.appendChild(rating);
+        
+        desCard.append(colorSelector, tagDiv, nameDiv, priceDiv,ratingDiv);
+
+        card.append(imgcard, desCard);
+        parent.appendChild(card)
+    })
+}
+
+
+function sorting() {
+    let sortValue = document.getElementById("sort").value;
+    switch (sortValue) {
+        case "1":
+            sortOriginal();
+            break;
+        case "2":
+            sortRating();
+            break;
+        case "3":
+            sortphl();
+            break;
+        
+        case "4":
+            sortplh();
+            break
+    }
+        
+}
+
+function sortOriginal() {
+    parent.innerHTML = null;
+    appendItem(original)
+}
+
+
+function sortRating() {
+    parent.innerHTML = null;
+    let newData = JSON.parse(localStorage.getItem('clothes'));
+    newData.sort(function (a, b) {
+        return a.rating - b.rating;
+    })
+    appendItem(newData)
+    
+}
+function sortplh() {
+    parent.innerHTML = null;
+    let newData = JSON.parse(localStorage.getItem('clothes'));
+    newData.sort(function (a, b) {
+        return a.price - b.price;
+    })
+    appendItem(newData)
+    
+}
+
+function sortphl() {
+    parent.innerHTML = null;
+    let newData = JSON.parse(localStorage.getItem('clothes'));
+    newData.sort(function (a, b) {
+        return b.price - a.price;
+    })
+    appendItem(newData)
+    
+}
+
+
+let sort = document.getElementById('sort');
+sort.onclick = function () {
+    sorting()
+};
+
+let itemavailableP = document.getElementById('item-available');
+itemavailableP.innerHTML = `${itemavailable} items`;
